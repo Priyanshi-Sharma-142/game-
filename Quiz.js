1a619ef31bb4334b44c40351ba72ff1c049c38a6
Priyanshi-Sharma-142 <71590973+Priyanshi-Sharma-142@users.noreply.github.com>
2020-11-07 22:50:47 +0530

    Updated quiz.js

--- Quiz.js
+++ Quiz.js
@@ -1,160 +1,18 @@
-const restartBtn = document.getElementById('restart');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
-const submitBtn = document.getElementById('submit');
-const trueBtn = document.getElementById('True');
-const falseBtn = document.getElementById('False');
-const userScore = document.getElementById('user-score');
-const quesText = document.getElementById('ques-text');
-let currentQues = 0;
-var score = 0;
-let questions = [
-    {
-        question:"Taj Mahal is in Agra.",
-        answers:[
-            {option:"True",answer:true},
-            {option:"No, not at all.",answer:false}
-        ]
-    },
-    {
-        question:"Complete the phrase: Every cloud has _____",
-        answers:[
-            {option:"Sliver lining",answer:true},
-            {option:"Rainbow",answer:false}
-        ]
-    },
-    {
-        question:"Did you like the background of this webpage? ",
-        answers:[
-            {option:"Bad",answer:false},
-            {option:"Awesome",answer:true}
-        ]
-    }
-]
-
-restartBtn.addEventListener('click',restart);
-prevBtn.addEventListener('click',prev);
-nextBtn.addEventListener('click',next);
-submitBtn.addEventListener('click',submit);
-
-function beginQuiz(){
-    currentQues = 0;
-    quesText.innerHTML = questions[currentQues].question;
-    trueBtn.innerHTML = questions[currentQues].answers[0].option;
-    trueBtn.onclick = () => {
-        let ano=0;
-        if(questions[currentQues].answers[ano].answer){
-            if(score<3){
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQues<2){
-            next();
-        }
-    }
-    falseBtn.innerHTML = questions[currentQues].answers[1].option;
-    falseBtn.onclick = () => {
-        let ano=1;
-        if(questions[currentQues].answers[ano].answer){
-            if(score<3){
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQues<2){
-            next();
-        }
-    }
-    prevBtn.classList.add('hide');
-}
-beginQuiz();
-function restart(){
-    currentQues = 0;
-    prevBtn.classList.remove('hide');
-    nextBtn.classList.remove('hide');
-    submitBtn.classList.remove('hide');
-    trueBtn.classList.remove('hide');
-    falseBtn.classList.remove('hide');
-    score = 0;
-    userScore.innerHTML = score;
-    beginQuiz();
-}
-function next(){
-    currentQues++;
-    if(currentQues>=2){
-        nextBtn.classList.add('hide');
-        prevBtn.classList.remove('hide');
-    }
-    quesText.innerHTML = questions[currentQues].question;
-    trueBtn.innerHTML = questions[currentQues].answers[0].option;
-    trueBtn.onclick = ()=>{
-        let ano=0;
-        if(questions[currentQues].answers[ano].answer){
-            if(score<3){
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQues<2){
-            next();
-        }
-    }
-    falseBtn.innerHTML = questions[currentQues].answers[1].option;
-    falseBtn.onclick = ()=>{
-        let ano=1;
-        if(questions[currentQues].answers[ano].answer){
-            if(score<3){
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQues<2){
-            next();
-        }
-    }
-    prevBtn.classList.remove('hide');
-}
-function prev(){
-    currentQues--;
-    if(currentQues<=0){
-        prevBtn.classList.add('hide');
-        nextBtn.classList.remove('hide');
-    }
-    quesText.innerHTML = questions[currentQues].question;
-    trueBtn.innerHTML = questions[currentQues].answers[0].option;
-    trueBtn.onclick = ()=>{
-        let ano=0;
-        if(questions[currentQues].answers[ano].answer){
-            if(score<3){
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQues<2){
-            next();
-        }
-    }
-    falseBtn.innerHTML = questions[currentQues].answers[1].option;
-    falseBtn.onclick = ()=>{
-        let ano=1;
-        if(questions[currentQues].answers[ano].answer){
-            if(score<3){
-                score++;
-            }
-        }
-        userScore.innerHTML = score;
-        if(currentQues<2){
-            next();
-        }
-    }
-    nextBtn.classList.remove('hide');
-}
-function submit(){
-    prevBtn.classList.add('hide');
-    nextBtn.classList.add('hide');
-    submitBtn.classList.add('hide');
-    trueBtn.classList.add('hide');
-    falseBtn.classList.add('hide');
-    quesText.innerHTML="Congratulations on submitting the quiz!";
+ function check(){
+    var c=0;
+    var q1=document.quiz.ques1.value;
+    var q2=document.quiz.ques2.value;
+    var q3=document.quiz.ques3.value;
+    var q4=document.quiz.ques4.value;
+    var q5=document.quiz.ques5.value;
+    var result=document.getElementById('result');
+    var quiz=document.getElementById('quiz');
+    if(q1=="C++") {c++}
+    if(q2=="py") {c++}
+    if(q3=="int") {c++}
+    if(q4=="in") {c++}
+if(q5=="1") {c++}
+    quiz.style.display="none";
+    result.innerHTML=`RESULT: <br /><br /> You scored ${c}/2 <br /><br /> Congratulations! <br /><br />`;
+    result.innerHTML+=`<input type="button" name="" value="Try Again!" id="button1" onclick="window.location.reload()">`;
 }
